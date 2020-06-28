2957574b69448cbf861ab78b9cf6583b22181703
matwright <mat.wright@broadshout.com>
2020-06-29 00:21:03 +0200

    update name

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'FlutterBand',
         darkTheme: ThemeData(
           brightness: Brightness.dark,
         ),
