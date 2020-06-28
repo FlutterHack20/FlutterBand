645fe386946aecdd893cef290992a2a8b0a0dd07
Rui Lima <ruipedro.lima@gmail.com>
2020-06-28 13:55:40 +0100

    Added the knob again

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,6 +15,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
 
     return
       BlocBuilder<NavBloc, NavState>(
@@ -27,55 +28,60 @@ extendBodyBehindAppBar: false,
         }
       );
 
+=======
+    return BlocBuilder<NavBloc, NavState>(builder: (_, state) {
+      return Scaffold(
+          appBar: AppBar(title: Text('Flutterband v.1.0.beta')),
+          extendBodyBehindAppBar: false,
+          body: _body(state, context));
+    });
+>>>>>>> Added the knob again
   }
 }
 
-_body(NavState state,BuildContext context){
-  if(state is InitialNavState || state is HomeNavState){
-
+_body(NavState state, BuildContext context) {
+  if (state is InitialNavState || state is HomeNavState) {
     return Column(
       children: <Widget>[
         Padding(padding: EdgeInsets.all(20)),
-        Image(image:AssetImage('assets/logo.png') ),
+        Image(image: AssetImage('assets/logo.png')),
         Padding(padding: EdgeInsets.all(20)),
         ButtonTheme(
-          minWidth: 200.0,
-          height: 50.0,
-          buttonColor: Color.fromRGBO(67, 132, 165,1),
-          shape:  RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-          side: BorderSide(width: 1.00,color: Color.fromRGBO(45, 92, 110,1))),
-          textTheme: ButtonTextTheme.primary,
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  BlocProvider.of<HomeBloc>(context).add(StartBroadcastEvent())
-                },
-                child: Text('Broadcast'),
-              ),
-              Padding(padding:EdgeInsets.only(top: 10),),
-              RaisedButton(
-                onPressed: () {
-                  BlocProvider.of<HomeBloc>(context).add(
-                    StartIncomingEvent(
-                      Message(
-                        message: 'Hallo wie geht\'s dir?'
-                      ),
-                      Localizations.localeOf(context).languageCode
-                    )
-                  );
-                }, child: Text('Listen')
-              ),
-            ],
-          )
-        )
+            minWidth: 200.0,
+            height: 50.0,
+            buttonColor: Color.fromRGBO(67, 132, 165, 1),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                side: BorderSide(
+                    width: 1.00, color: Color.fromRGBO(45, 92, 110, 1))),
+            textTheme: ButtonTextTheme.primary,
+            child: Column(
+              children: <Widget>[
+                CyberKnob(),
+                RaisedButton(
+                  onPressed: () {
+                    BlocProvider.of<HomeBloc>(context)
+                        .add(StartBroadcastEvent());
+                  },
+                  child: Text('Broadcast'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                ),
+                RaisedButton(
+                    onPressed: () {
+                      BlocProvider.of<HomeBloc>(context).add(StartIncomingEvent(
+                          Message(message: 'Hallo wie geht\'s dir?'),
+                          Localizations.localeOf(context).languageCode));
+                    },
+                    child: Text('Listen')),
+              ],
+            ))
       ],
     );
 
 
 
   }
-
 }
 
