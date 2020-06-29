167ec338b5e829d3726a9182ad0ae787ffd3564d
matwright <mat.wright@broadshout.com>
2020-06-29 14:07:31 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/widgets/channel_display_widget.dart

--- lib/widgets/channel_display_widget.dart
+++ lib/widgets/channel_display_widget.dart
@@ -6,8 +6,7 @@ import 'package:flutterband/blocs/home/bloc.dart';
 class ChannelDisplayWidget extends StatelessWidget {
 
 
-
-  int currentChannel=1;
+  final int currentChannel = 1;
 
   ChannelDisplayWidget({
     Key key,
@@ -17,11 +16,12 @@ class ChannelDisplayWidget extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
+      var channel = state.channel ?? currentChannel;
       return Expanded(
           child: Container(
               alignment: Alignment.center,
               child: Text(
-                  state.channel.toString(),
+                  channel.toString(),
                   style: TextStyle(
                       color: Colors.lightBlueAccent,
                       fontSize: 200,
--- lib/widgets/speak.dart
+++ lib/widgets/speak.dart
@@ -32,7 +32,7 @@ class _SpeakState extends State<Speak> {
           BlocProvider.of<HomeBloc>(context).add(StartBroadcastEvent(currentChannel()));
         });
       },
-      child: new FlareActor("assets/flares/recordButton_wIcon.flr",
+      child: new FlareActor("assets/flares/MicButton.flr",
           alignment: Alignment.center,
           fit: BoxFit.contain,
           controller: controls,
